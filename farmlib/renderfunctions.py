f236543fbd5c6f0b52f939374ff997ad1d687054
Marcin orneo1212 <orneo1212@gmail.com>
2012-05-23 12:24:33 +0200

    Code refactor

---
+++ farmlib/renderfunctions.py
@@ -0,0 +1,126 @@
+'''
+Created on 23-05-2012
+
+@author: orneo1212
+'''
+import pygame
+
+def draw_tools(surface, currenttool, currentseed, imgloader):
+    #Draw selection on selected tool
+    if currenttool == 'harvest':
+        pygame.draw.rect(surface, (255, 255, 255), (10, 10, 48, 48), 1)
+    if currenttool == 'plant':
+        pygame.draw.rect(surface, (255, 255, 255), (60, 10, 48, 48), 1)
+    if currenttool == 'watering':
+        pygame.draw.rect(surface, (255, 255, 255), (110, 10, 48, 48), 1)
+
+    mx, my = pygame.mouse.get_pos()
+
+    #Draw current tool
+    if currenttool == "plant":
+        img = imgloader.loadimage('seed' + str(currentseed))
+    if currenttool == "harvest":
+        img = imgloader.loadimage('sickle')
+    if currenttool == "watering":
+        img = imgloader.loadimage('wateringcan')
+    surface.blit(img, (mx, my - 48))
+
+    #draw tools
+    #SICKLE Harvest (10,10,48,48)
+    #PLANT (60,10,48,48)
+    surface.blit(imgloader.loadimage('sickle'), (10, 10))
+    surface.blit(imgloader.loadimage('plant'), (60, 10))
+    surface.blit(imgloader.loadimage('wateringcan'), (110, 10))
+
+def draw_seed(surface, seedid, position, imgloader):
+        img = imgloader.loadimage('seed' + str(seedid))
+        surface.blit(img, position)
+
+def draw_selected_seed(surface, selectedseed, imgloader):
+    #draw selected seed
+    img = imgloader.loadimage('dryground')
+    surface.blit(img, (65, 65))
+    draw_seed(surface, selectedseed, (65, 65), imgloader)
+
+def render_seed_notify(surface, font, posx, posy, underseed, imgloader):
+    """Render notification about planted seed"""
+
+    sizex = 200
+    sizey = 150
+    posy += 5
+
+    img = pygame.Surface((sizex, sizey))
+    img.fill((48, 80, 80))
+    pygame.draw.rect(img, (255, 255, 255), (0, 0, sizex - 1, sizey - 1), 1)
+
+    #Draw seed
+    draw_seed(img, underseed.id, (sizex / 2 - 32, 65), imgloader)
+
+    #name
+    text = "" + underseed.name + ""
+    text = font.render(text, 0, (255, 255, 0), (255, 0, 255))
+    text.set_colorkey((255, 0, 255))
+    img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 5))
+
+    #remaining time
+    text = "Complete in: " + underseed.remainstring
+    text = font.render(text, 0, (255, 0, 100), (255, 0, 255))
+    text.set_colorkey((255, 0, 255))
+    img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 25))
+
+    #Quentity
+    text = "Quantity:" + str(underseed.growquantity)
+    text = font.render(text, 0, (255, 255, 150), (255, 0, 255))
+    text.set_colorkey((255, 0, 255))
+    img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 45))
+
+    #ready to harvest
+    if underseed.to_harvest:
+        text = font.render("Ready to Harvest", 0, (255, 255, 255), (255, 0, 255))
+        text.set_colorkey((255, 0, 255))
+        img.blit(text, (sizex / 2 - text.get_size()[0] / 2, sizey - 20))
+
+    #alpha
+    img.set_alpha(128 + 64)
+    if posx > (640 - sizex):posx -= sizex
+    surface.blit(img, (posx, posy))
+
+
+def generate_field_sprites(imgloader, farmfield, farmoffset):
+    group = pygame.sprite.OrderedUpdates()
+    #background
+    sprite = pygame.sprite.Sprite()
+    sprite.image = imgloader['background']
+    sprite.rect = (0, 0, 800, 600)
+    group.add(sprite)
+
+    for y in range(12):
+        for x in range(12):
+            farmtile = farmfield.get_farmtile(x, y)
+
+            posx = (x - y) * 32 + farmoffset[0] + 150
+            posy = (x + y) * 16 + farmoffset[1]
+
+            #draw ground
+            sprite = pygame.sprite.Sprite()
+            sprite.rect = (posx, posy, 64, 32)
+            if farmtile['water'] > 20:
+                sprite.image = imgloader['wetground']
+            else:
+                sprite.image = imgloader['dryground']
+            group.add(sprite)
+
+            #draw plant or seed
+            seed = farmtile['seed']
+
+            if seed:
+                sprite = pygame.sprite.Sprite()
+                sprite.rect = (posx, posy, 64, 32)
+                if not seed.to_harvest:
+                    sprite.image = imgloader['seed']
+                else:
+                    sprite.image = imgloader['seed' + str(seed.id)]
+                #add sprite
+                group.add(sprite)
+    #return group object
+    return group
--- pyFarmGame.py
+++ pyFarmGame.py
@@ -8,6 +8,7 @@ from farmlib.inventory import PygameInventory
 from farmlib.player import Player
 from farmlib.window import Window
 from farmlib.widgetlabel import Label
+from farmlib.renderfunctions import *
 
 pygame.init()
 
@@ -34,7 +35,7 @@ class FarmGamePygame:
         self.farm = FarmField()
         self.timer = pygame.time.Clock()
 
-        self.groups = [pygame.sprite.OrderedUpdates()] # view groups
+        self.groups = [None] # view groups
         self.images = ImageLoader(imagesdata)
         self.notifyfont = pygame.font.Font("droidsansmono.ttf", 12)
         self.font2 = pygame.font.Font("droidsansmono.ttf", 18)
@@ -66,6 +67,13 @@ class FarmGamePygame:
         modified = self.farm.update()
         if modified:self.generate_field_sprites()
 
+    def regenerate_groups(self):
+        self.groups[0] = generate_field_sprites(
+                                                self.images,
+                                                self.farm,
+                                                self.farmoffset
+                                                )
+
     def handle_farmfield_events(self, event):
         #Mouse motion
         mx, my = pygame.mouse.get_pos()
@@ -80,12 +88,12 @@ class FarmGamePygame:
                 if self.currenttool == 'harvest' and pos:
                     self.farm.harvest(pos[0], pos[1], self.player)
                     #regenerate sprites
-                    self.generate_field_sprites()
+                    self.regenerate_groups()
 
                 if self.currenttool == 'watering' and pos:
                     self.farm.water(pos[0], pos[1])
                     #regenerate sprites
-                    self.generate_field_sprites()
+                    self.regenerate_groups()
 
             #there no seed under mouse
             else:
@@ -94,7 +102,7 @@ class FarmGamePygame:
                         self.player.create_new_seed_by_id(self.currentseed)
                         )
                     #regenerate sprites
-                    self.generate_field_sprites()
+                    self.regenerate_groups()
 
             #events for inventory
             index = self.inventory.get_index_inventory_under_mouse()
@@ -104,21 +112,21 @@ class FarmGamePygame:
                     self.currentseed = self.player.inventory[itemid]
                     self.currenttool = 'plant'
                     #regenerate sprites
-                    self.generate_field_sprites()
+                    self.regenerate_groups()
 
             #events for tools
             if pygame.Rect((10, 10, 48, 48)).collidepoint((mx, my)):
                 self.currenttool = 'harvest'
                 #regenerate sprites
-                self.generate_field_sprites()
+                self.regenerate_groups()
             if pygame.Rect((60, 10, 48, 48)).collidepoint((mx, my)):
                 self.currenttool = 'plant'
                 #regenerate sprites
-                self.generate_field_sprites()
+                self.regenerate_groups()
             if pygame.Rect((110, 10, 48, 48)).collidepoint((mx, my)):
                 self.currenttool = 'watering'
                 #regenerate sprites
-                self.generate_field_sprites()
+                self.regenerate_groups()
 
     def events(self):
         """Events handler"""
@@ -150,105 +158,19 @@ class FarmGamePygame:
             if not self.sellwindow.visible:
                 self.handle_farmfield_events(event)
 
-    def generate_field_sprites(self):
-        group = self.groups[0]
-        group.empty()
-
-        #background
-        sprite = pygame.sprite.Sprite()
-        sprite.image = self.images.loadimage('background')
-        sprite.rect = (0, 0, 800, 600)
-        group.add(sprite)
-
-        #frame
-        #sprite=pygame.sprite.Sprite()
-        #sprite.image=self.images.loadimage('frame')
-        #sprite.rect=(
-        #    self.farmoffset[0]-30,
-        #    self.farmoffset[1]-30,
-        #    800,
-        #    600
-        #    )
-        #group.add(sprite)
-
-        for y in range(12):
-            for x in range(12):
-                farmtile = self.farm.get_farmtile(x, y)
-
-                posx = (x - y) * 32 + self.farmoffset[0] + 150
-                posy = (x + y) * 16 + self.farmoffset[1]
-
-                #draw ground
-                sprite = pygame.sprite.Sprite()
-                sprite.rect = (posx, posy, 64, 32)
-                if farmtile['water'] > 20:
-                    sprite.image = self.images['wetground']
-                else:
-                    sprite.image = self.images['dryground']
-                group.add(sprite)
-
-                #draw plant or seed
-                seed = farmtile['seed']
-
-                if seed:
-                    sprite = pygame.sprite.Sprite()
-                    sprite.rect = (posx, posy, 64, 32)
-                    if not seed.to_harvest:
-                        sprite.image = self.images['seed']
-                    else:
-                        sprite.image = self.images['seed' + str(seed.id)]
-                    #add sprite
-                    group.add(sprite)
-
-    def draw_tools(self, surface):
-        #Draw selection on selected tool
-        if self.currenttool == 'harvest':
-            pygame.draw.rect(surface, (255, 255, 255), (10, 10, 48, 48), 1)
-        if self.currenttool == 'plant':
-            pygame.draw.rect(surface, (255, 255, 255), (60, 10, 48, 48), 1)
-        if self.currenttool == 'watering':
-            pygame.draw.rect(surface, (255, 255, 255), (110, 10, 48, 48), 1)
-
-        mx, my = pygame.mouse.get_pos()
-
-        #Draw current tool
-        if self.currenttool == "plant":
-            img = self.images.loadimage('seed' + str(self.currentseed))
-        if self.currenttool == "harvest":
-            img = self.images.loadimage('sickle')
-        if self.currenttool == "watering":
-            img = self.images.loadimage('wateringcan')
-        surface.blit(img, (mx, my - 48))
-
-    def draw_seed(self, surface, seedid, position):
-        img = self.images.loadimage('seed' + str(seedid))
-        surface.blit(img, position)
-
-    def draw_selected_seed(self, surface):
-        #draw selected seed
-        img = self.images.loadimage('dryground')
-        surface.blit(img, (65, 65))
-        self.draw_seed(surface, self.currentseed, (65, 65))
-
     def redraw(self, screen):
         """Redraw screen"""
 
         #Draw Farmfeld
         self.groups[0].draw(screen)
 
-        #draw tools
-        #SICKLE Harvest (10,10,48,48)
-        #PLANT (60,10,48,48)
-        screen.blit(self.images.loadimage('sickle'), (10, 10))
-        screen.blit(self.images.loadimage('plant'), (60, 10))
-        screen.blit(self.images.loadimage('wateringcan'), (110, 10))
-
         text = "Money:%s" % self.player.money
         text = self.font2.render(text, 1, (255, 255, 255))
         text.set_colorkey((255, 0, 255))
         screen.blit(text, (400 - text.get_size()[0] / 2, 5))
 
-        self.draw_tools(screen)
+        #Draw tools and selected tool rectangle
+        draw_tools(screen, self.currenttool, self.currentseed, self.images)
 
         if not self.sellwindow.visible:
             self.inventory.draw_inventory(screen, self.player)
@@ -260,59 +182,23 @@ class FarmGamePygame:
             if pos:
                 seed = self.farm.get_farmtile(pos[0], pos[1])['seed']
                 if seed:
-                    self.render_notify(screen, mx + 5, my + 5, seed)
+                    render_seed_notify(
+                                       screen,
+                                       self.notifyfont,
+                                       mx + 5,
+                                       my + 5,
+                                       seed,
+                                       self.images
+                                      )
             #draw inventory
             self.inventory.draw_inventory_notify(self.screen, self.player)
 
-        self.draw_selected_seed(screen)
+        draw_selected_seed(screen, self.currentseed, self.images)
 
         #redraw sell window
         self.sellwindow.render(screen, (200, 40))
         #update screen
         pygame.display.flip()
-
-    def render_notify(self, screen, posx, posy, underseed):
-        """Render notification about planted seed"""
-
-        sizex = 200
-        sizey = 150
-        posy += 5
-
-        img = pygame.Surface((sizex, sizey))
-        img.fill((48, 80, 80))
-        pygame.draw.rect(img, (255, 255, 255), (0, 0, sizex - 1, sizey - 1), 1)
-
-        #Draw seed
-        self.draw_seed(img, underseed.id, (sizex / 2 - 32, 65))
-
-        #name
-        text = "" + underseed.name + ""
-        text = self.notifyfont.render(text, 0, (255, 255, 0), (255, 0, 255))
-        text.set_colorkey((255, 0, 255))
-        img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 5))
-
-        #remaining time
-        text = "Complete in: " + underseed.remainstring
-        text = self.notifyfont.render(text, 0, (255, 0, 100), (255, 0, 255))
-        text.set_colorkey((255, 0, 255))
-        img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 25))
-
-        #Quentity
-        text = "Quantity:" + str(underseed.growquantity)
-        text = self.notifyfont.render(text, 0, (255, 255, 150), (255, 0, 255))
-        text.set_colorkey((255, 0, 255))
-        img.blit(text, (sizex / 2 - text.get_size()[0] / 2, 45))
-
-        #ready to harvest
-        if underseed.to_harvest:
-            text = self.notifyfont.render("Ready to Harvest", 0, (255, 255, 255), (255, 0, 255))
-            text.set_colorkey((255, 0, 255))
-            img.blit(text, (sizex / 2 - text.get_size()[0] / 2, sizey - 20))
-
-        #alpha
-        img.set_alpha(128 + 64)
-        if posx > (640 - sizex):posx -= sizex
-        screen.blit(img, (posx, posy))
 
     def get_farmtile_pos_under_mouse(self):
         """Get FarmTile position under mouse"""
@@ -356,7 +242,8 @@ class FarmGamePygame:
         result = self.farm.load_farmfield('field.xml', self.player)
         if not result:print "No save game found. Starting new one"
 
-        self.generate_field_sprites()
+        self.regenerate_groups()
+
         while self.running:
             self.events()
             self.update()
