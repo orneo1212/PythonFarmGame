47570bf70135f664f28751d87bdb6272d6b2e7ec
Marcin orneo1212 <orneo1212@gmail.com>
2012-05-25 12:04:10 +0200

    message label, cost label and costvalue label for market

--- farmlib/marketwindow.py
+++ farmlib/marketwindow.py
@@ -15,21 +15,43 @@ class MarketWindow(Window):
         self.player = player
         self.imgloader = imgloader
         Window.__init__(self, size, (200, 40))
-        titlelabel = Label(
-                           "Market place",
-                           (200, 0),
-                            size = 18,
-                            color = (255, 255, 0),
-                            align = "center")
-        self.addwidget(titlelabel)
 
+        #items offset for gui buttons
+        self.itemsoffset = [32, 20]
+
+        #Create gui
+        self.create_gui()
+
+
+        #hide market at load
+        self.hide()
+
+    def create_gui(self):
+        #Title 
+        titlelabel = Label("Market place", (200, 0), size = 18,
+                           color = (255, 255, 0), align = "center")
+        self.addwidget(titlelabel)
+        #Costlabel
+        costlabel = Label("Buy cost:", (80, 340), size = 12,
+                           color = (200, 0, 200), align = "center")
+        self.addwidget(costlabel)
+        #Cost value
+        self.costvalue = Label("0", (120, 340), size = 12,
+                           color = (200, 200, 50), align = "center")
+        self.addwidget(self.costvalue)
+        #Message
+        self.message = Label("", (10, 360), size = 12,
+                           color = (255, 0, 255), align = "center")
+        self.addwidget(self.message)
         #Add images for seeds in market
         posx, posy = [0, 0]
-        offset = [32, 20]
+        columns = (self.width / 64)
+        rows = (self.height / 32)
         for seeddef in seeds:
-            img = imgloader['seed' + str(seeddef['id'])]
-            px = self.width / 2 * posx + offset[0]
-            py = self.height / 6 * posy + offset[1]
+            #add seed image widget 
+            img = self.imgloader['seed' + str(seeddef['id'])]
+            px = self.width / columns * posx + self.itemsoffset[0]
+            py = self.height / rows * posy + self.itemsoffset[1]
             image = Image(img, (px, py))
             self.addwidget(image)
             posx += 1
@@ -43,12 +65,12 @@ class MarketWindow(Window):
         self.addwidget(self.sellbutton)
         self.buybutton.connect("clicked", self.on_buy_clicked)
         self.sellbutton.connect("clicked", self.on_sell_clicked)
-        #hide market at load
-        self.hide()
 
     def on_buy_clicked(self, widget, **data):
         self.player.money -= 5
         self.player.add_item(0)
+        self.message.settext("You bought carrot")
 
     def on_sell_clicked(self, widget, **data):
         self.player.money += 5
+        self.message.settext("You sold carrot")
