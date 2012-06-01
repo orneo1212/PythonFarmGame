3bb2ed4bd3ed724166e3d0616bceef7a0b052bbc
Marcin orneo1212 <orneo1212@gmail.com>
2012-06-01 12:39:57 +0200

    updated changelog and version

--- farmlib/__init__.py
+++ farmlib/__init__.py
@@ -14,12 +14,13 @@ from dictmapper import DictMapper
 
 
 #SETTINGS
-__VERSION__ = "0.4.4"
 
 rules = DictMapper()
 rules.load(os.path.join("data", "rules.json"))
 
 images = DictMapper()
 images.load(os.path.join("data", "images.json"))
+
+__VERSION__ = rules["VERSION"]
 
 from pluginsystem import basePluginSystem as PluginSystem
